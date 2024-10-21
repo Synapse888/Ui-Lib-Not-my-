bcb954bdf7f0be5f570164b86d105be6e7ceee30
Synapse888 <egorixios@gmail.com>
2024-10-21 11:15:51 +0300

    Example.lua

--- Example.lua
+++ Example.lua
@@ -1,12 +1,14 @@
-local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/cat"))() --you can go into the github link and copy all of it and modify it for yourself.
-local Window = Library:CreateWindow("cattoware UI Doc", Vector2.new(492, 598), Enum.KeyCode.RightControl) --you can change your UI keybind
-local AimingTab = Window:CreateTab("Tab 1") --you can rename this tab to whatever you want --you can also change the tabs code, for example "AimingTab" can be changed to "FunnyCoolTab" etc.
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/cat"))()
+local Window = Library:CreateWindow("cattoware UI Doc", Vector2.new(492, 598), Enum.KeyCode.RightControl)
+local AimingTab = Window:CreateTab("Tab 1")
 
 
 local testSection = AimingTab:CreateSector("First Section", "left")  --you can  change the section code, for example "testsection" can be changed to "FunnyCoolSection" etc.
 
+local label = testSection:AddLabel("Label")
+
 testSection:AddToggle("Toggle", false, function(first)
-    print("ejejejejejeje")
+    print("togle")
 end)
 
 testSection:AddButton("Button", function(IhateGayPeople)
@@ -14,15 +16,15 @@ testSection:AddButton("Button", function(IhateGayPeople)
 end)
 
 testSection:AddSlider("Slider", 0, 120, 2000, 1, function(State)
-    
+  print("slider")  
 end)
 
 testSection:AddTextbox("textbox", nil, function(State)
-
+print("textbox")
 end)
 
 testSection:AddDropdown("Dropdown", {"AH", "ytytyt", "rrro", "awhjdajksd"}, "AH", true, function(dropdown)
-
+print("dropdown")
 end)
 
 local ColorToggle = testSection:AddToggle("ColorPicker w/Toggle", false, function(e)
